86253a44f699fffd75c506b593671ceaff6faaf5
Pavlo Ratushnyi <pavel@apination.com>
2020-05-09 02:49:03 +0300

    Fixed library

--- esp32-pwm.cpp
+++ esp32-pwm.cpp
@@ -1,59 +1,57 @@
 #include "esp32-pwm.h"
 #include <Arduino.h>
-#include <math.h>  
-
-ESP32_PWM::ESP32_PWM(uint8_t pin)
-{
-    ESP32_PWM(pin, CHANNEL_DEFAULT);
-}
-ESP32_PWM::ESP32_PWM(uint8_t pin, uint8_t channel)
-{
-    ESP32_PWM(pin, channel, STEPS_DEFAULT);
-}
-
-ESP32_PWM::ESP32_PWM(uint8_t pin, uint8_t channel, uint8_t steps)
-{
-    ESP32_PWM(pin, channel, steps, MODE_DEFAULT);
-}
-
-ESP32_PWM::ESP32_PWM(uint8_t pin, uint8_t channel, uint8_t steps, uint8_t mode)
-{
-    ESP32_PWM(pin, channel, steps, mode, SPEED_DEFAULT);
-}
+#include <math.h>
 
 ESP32_PWM::ESP32_PWM(uint8_t pin, uint8_t channel, uint8_t steps, uint8_t mode, uint32_t speed)
 {
-    this->pin = pin;
-    this->channel = channel;
-    this->steps = steps;
-    this->mode = mode;
-    this->speed = speed;
+    _pin = pin;
+#ifdef DEBUG
+    Serial.print("Pin: ");
+    Serial.println(_pin);
+#endif
+    _channel = channel;
+#ifdef DEBUG
+    Serial.print("Channel: ");
+    Serial.println(_channel);
+#endif
+    _steps = steps;
+    _mode = mode;
+    _speed = speed;
 
     this->init();
 }
+
 void ESP32_PWM::init()
 {
-
-    pinMode(this->pin, OUTPUT);
-    // configure LED PWM functionalitites
-    ledcSetup(this->channel, this->speed, 8);
-
-    // attach the channel to the GPIO2 to be controlled
-    ledcAttachPin(this->pin, this->channel);
+    pinMode(_pin, OUTPUT);
+    ledcSetup(_channel, _speed, PWM_PREC_BIT);
+    ledcAttachPin(_pin, _channel);
 }
 
 uint8_t ESP32_PWM::setBrightness(uint8_t value)
 {
-    value = constrain(value, 0, this->steps - 1);
-    int percent = (int)100 * value / this->steps;
+#ifdef DEBUG
+    Serial.print("Input value:");
+    Serial.println(value);
+#endif
+    value = constrain(value, 0, _steps - 1);
+#ifdef DEBUG
+    Serial.print("Normalized value:");
+    Serial.println(value);
+#endif
+    int percent = (int)100 * value / _steps;
+#ifdef DEBUG
+    Serial.print("Percent:");
+    Serial.println(percent);
+#endif   
     int v_min = 0;
-    int v_max = 2 ^ 8 - 1;
+    int v_max = 2 ^ PWM_PREC_BIT - 1;
     int v_value = (int)100 * percent / v_max;
-    if (this->mode == MODE_LOG)
+    if (_mode == MODE_LOG)
     {
-      float a = 0.4;//0.33..0.5
-        v_value = (int)pow(v_max*percent/100,1/a)+0.5;
+        float a = 0.4; //0.33..0.5
+        v_value = (int)pow(v_max * percent / 100, 1 / a) + 0.5;
     }
-    ledcWrite(this->channel, v_value);
+    ledcWrite(_channel, v_value);
     return v_value;
 }
--- esp32-pwm.h
+++ esp32-pwm.h
@@ -1,30 +1,30 @@
 #pragma once
 #include <Arduino.h>
 
-#define MODE_LINEAR 1
-#define MODE_LOG 2
+#define MODE_LINEAR             1
+#define MODE_LOG                2
 
-#define MODE_DEFAULT MODE_LOG
+#define MODE_DEFAULT            MODE_LOG
 
-#define SPEED_DEFAULT 5000
-#define STEPS_DEFAULT 100
-#define CHANNEL_DEFAULT 0
+#define SPEED_DEFAULT           5000
+#define STEPS_DEFAULT           100
+#define CHANNEL_DEFAULT         0
+
+#define PWM_PREC_BIT            10
+#define DEBUG
 
 class ESP32_PWM
 {
 private:
-    uint8_t pin;
-    uint8_t channel;
-    uint8_t steps;
-    uint8_t mode;
-    uint32_t speed;
+    uint8_t     _pin;
+    uint8_t     _channel;
+    uint8_t     _steps;
+    uint8_t     _mode;
+    uint32_t    _speed;
+
     void init();
 
 public:
-    ESP32_PWM(uint8_t pin);
-    ESP32_PWM(uint8_t pin, uint8_t channel);
-    ESP32_PWM(uint8_t pin, uint8_t channel, uint8_t steps);
-    ESP32_PWM(uint8_t pin, uint8_t channel, uint8_t steps, uint8_t mode);
-    ESP32_PWM(uint8_t pin, uint8_t channel, uint8_t steps, uint8_t mode, uint32_t speed);
+    ESP32_PWM(uint8_t pin, uint8_t channel = CHANNEL_DEFAULT, uint8_t steps = STEPS_DEFAULT, uint8_t mode = MODE_DEFAULT, uint32_t speed = SPEED_DEFAULT);
     uint8_t setBrightness(uint8_t value);
 };
